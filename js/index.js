37cb079146fb2088a004518c3ec12ba927f0b561
egierdian <egierdian1@gmail.com>
2020-12-03 07:57:45 +0700

    change

--- js/index.js
+++ js/index.js
@@ -112,24 +112,40 @@ const bodyHome = () => {
     `
     <section id="home">
         <div class="container">
-            <div class="card-body">
-                <img src="images/Profil.jpg" class="rounded-circle">
-                <h1 class="card-title display-5 diplay-name" id="Hello">Hi,<br /> i'm Egi Erdian 👋</h1>
-                <p class="card-text">
-                        My full name is Egi Erdian, people used to call me Egi or Erdian. I am 23 years old, and currently I live in cimahi. 
-                        I graduated from Vocational High School at SMK TI Pembangunan Cimahi with a major in software engineering. Currently I am continuing my education at Sekolah Tinggi Teknologi Bandung, majoring in computer science.  
-                </p>
-                <p class="card-text">
-                    Social Media
-                </p>
-                    
-                <div class="social-media">
-                    <a class="btn btn-dark" href="https://twitter.com/ErdianEGI"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark" href="https://fb.com/egi3rdian"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark" href="https://www.linkedin.com/in/egi-erdian-21a35a196/"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-dark" href="https://github.com/egierdian"><i class="fab fa-github"></i></a>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-12 col-12 mb-4">
+                            <img src="images/Profil.jpg" class="img-fluid">
+                        </div>
+                        <div class="offset-md-1 col-md-8 col-sm-12 col-12 mb-4">
+                            <h3 class="mb-3">
+                                <span class="badge badge-success">Hello</span>
+                            </h3>
+                            <h2 class="card-title display-5 diplay-name">i'm <strong>Egi Erdian</strong></h2>
+                                        
+                            <p class="card-text">
+                                    My full name is Egi Erdian, people used to call me Egi or Erdian. I am 23 years old, and currently I live in cimahi. 
+                                    I graduated from Vocational High School at SMK TI Pembangunan Cimahi with a major in software engineering. Currently I am continuing my education at Sekolah Tinggi Teknologi Bandung, majoring in computer science.  
+                            </p>
+                        </div>
+                    </div>
                 </div>
+                
             </div>
+            
+            <div class="social-media">
+                <ul>
+                    <li><a class="btn btn-dark" href="https://twitter.com/ErdianEGI"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </li>
+                    <li><a class="btn btn-dark" href="https://fb.com/egi3rdian"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li><a class="btn btn-dark" href="https://www.linkedin.com/in/egi-erdian-21a35a196/"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li><a class="btn btn-dark" href="https://github.com/egierdian"><i class="fa fa-github"></i></a>
+                    </li>
+                </ul>
+            </div>                        
         </div>
     </section>
     `;
@@ -165,7 +181,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/disawerin"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/disawerin"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="#"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -186,7 +202,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/at-bandung"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/at-bandung"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="https://at-bandung.web.app"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -208,7 +224,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/pwa-alquran-ku"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/pwa-alquran-ku"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="https://al-quran-ku.web.app"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -229,7 +245,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/e-loker"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/e-loker"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="https://e-loker.web.app/"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -250,7 +266,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/web-covid-19"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/web-covid-19"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="https://informasi-covid19.web.app/"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -271,7 +287,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="#"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="#"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="https://olivesbakery.com"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
@@ -289,7 +305,7 @@ const bodyPortfolio = () => {
                                             </div>
                                             <div class="btn-view">
                                                 <div class="portfolio-btn">
-                                                    <a class="btn btn-dark" href="https://github.com/egierdian/Tokobuku"><i class="fab fa-github"></i></a>
+                                                    <a class="btn btn-dark" href="https://github.com/egierdian/Tokobuku"><i class="fa fa-github"></i></a>
                                                     <a class="btn btn-dark" href="#"><i class="fa fa-eye"></i></a>
                                                 </div>
                                             </div>
